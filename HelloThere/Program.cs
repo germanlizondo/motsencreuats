7aaff6910b8d65ee8f50e863894baf68d5fc9c79
German <german9lp@gmail.com>
2018-11-12 20:37:00 +0100

    CON CLASE TABLERO

--- HelloThere/Program.cs
+++ HelloThere/Program.cs
@@ -13,59 +13,19 @@ namespace HelloThere
             int row = int.Parse(Console.ReadLine());
             Console.Write("¿Cuantas columnas quieres?\r\n");
             int column = int.Parse(Console.ReadLine());
-            char[] rows =new char[row];
-            char[] columns = new char[column];
 
 
-        
-            String word = "HELLO";
+            Tablero tablero = new Tablero(row, column);
 
-            Random rnd = new Random();
-            int randomnumber = rnd.Next(0,row);
-
-            char[] arrayword = word.ToCharArray();
-
-            foreach (char i in arrayword)
-            {
-                Console.WriteLine(i);
-            }
-
-
-            Console.WriteLine(randomnumber);
-
-            Console.WriteLine(arrayword[1]);
-
-            for (int x=0;x < row; x++)
-            {
-              
-                if (x == randomnumber)
-                {
-                    rows[x] = arrayword[1];
-                }
-                else { rows[x] = (char)x; }
-                
-            }
-
-            for (int x = 0; x < column; x++)
-            {
-                columns[x] = (char)x;
-            }
+            tablero.crearTablero();
+            tablero.dibujarTablero();
 
 
 
-
-
-
-            foreach (int y in rows)
-            {
-              
-            foreach (int x in columns)
-            {
-                System.Console.Write(" " + rows[x]);
-            }
-            System.Console.Write("\r\n");
-            }
+          
             Console.ReadLine();
+
+
         }
     }
 }
---
+++ HelloThere/Tablero.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloThere
+{
+  public class Tablero
+    {
+        private int row;
+        private int column;
+        private char[,] celdas;
+        private String todaslasletras = "abcdefghijklmnopqrstuvwxyz".ToUpper();
+      
+
+        public Tablero(int row,int column)
+        {
+            this.row = row;
+            this.column = row;
+            this.celdas = new char[this.row, this.column];
+
+        }
+
+        public void crearTablero()
+        {
+
+            int rowLength = this.celdas.GetLength(0);
+            int colLength = this.celdas.GetLength(1);
+
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    Random rnd = new Random();
+                    int randomnumber = rnd.Next(0, this.todaslasletras.Length);
+
+
+                    this.celdas[i, j] = this.todaslasletras[randomnumber];
+                }
+
+            }
+        }
+        public void dibujarTablero()
+        {
+            int rowLength = this.celdas.GetLength(0);
+            int colLength = this.celdas.GetLength(1);
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    Console.Write(this.celdas[i, j]);
+                }
+                Console.Write(Environment.NewLine);
+            }
+        }
+
+
+
+
+
+
+        //GETTER & SETTERS
+        public int Row {
+            get { return this.row; }
+            set { this.row = value; }
+        }
+        public int Column
+        {
+            get { return this.column; }
+            set { this.column = value; }
+        }
+
+        public char[,] Celdas
+        {
+            get { return this.celdas; }
+           
+        }
+    }
+}
